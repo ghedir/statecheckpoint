dd0d60490bb447c4637a79c991d4302e5ff1da92
ghedir <ghedirgahada77@gmail.com>
2021-05-19 14:57:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import myImage from "./images/profileImg.jpg";
+export default class App extends Component {
+  state = {
+    Person: {
+      fullName: "Ghada Gheir",
+      bio: "i just love coding",
+      profession: "Telecom Engineer",
+      imagesrc: myImage,
+    },
+    show: true,
+    count: 0,
+  };
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.show) {
+        this.setState({ count: this.state.count + 1 });
+      } else {
+        this.setState({ count: 0 });
+      }
+    }, 1000);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  toggle = () =>
+    this.setState((currentState) => ({ show: !currentState.show }));
+
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <div className="container">
+            <div className="card-container">
+              <div className="upper-container">
+                <div className="image-container">
+                  <img
+                    src={this.state.Person.imagesrc}
+                    alt=""
+                    style={{ height: "100px", width: "100px" }}
+                  />
+                </div>
+              </div>
+              <div className="lower-container">
+                <h3> FullName: {this.state.Person.fullName} </h3>
+                <h4> Profession: {this.state.Person.profession}</h4>
+                <p> Bio: {this.state.Person.bio} </p>
+                <h1>{this.state.count}</h1>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="btn">
+          <button onClick={this.toggle}>Click here</button>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
